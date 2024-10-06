35096f504766d156ca0a86fc74f8fa8fc60f49fa
Geky . <nathansleon@gmail.com>
2024-10-06 17:08:00 -0400

    Tabla Actualizada de alumnos LEM

--- Gestion_escolar_lem/settings.py
+++ Gestion_escolar_lem/settings.py
@@ -189,3 +189,21 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/nathan/Luis-Enrique-M-rmol-Servicio-Comunitario/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
